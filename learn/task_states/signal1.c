85853afcea27f996b50076265bcf6e73d7b2a25c
manish <manishsurolia@gmail.com>
2022-02-18 08:24:56 +0530

    Some notes added for signal1.c file

--- learn/task_states/signal1.c
+++ learn/task_states/signal1.c
@@ -11,6 +11,13 @@ void sig_handler(int num)
 int main(void)
 {
     signal(SIGINT, sig_handler);
+    printf("Calling getchar() is putting me in 'TASK_INTERRUPTABLE' state.\n");
+    printf("When I am in 'TASK_INTERRUPTABLE' state, any 'ctrl-c' signal to \n"
+           "me will put me back to the 'TASK_RUNNING' state and sig_handler \n"
+           "function will be called and then again I will be moved to \n"
+           "'TASK_INTERRUPTABLE' state. When an input required by 'getchar' \n"
+           "is passed to me, I will be again moved to 'TASK_RUNNING' state \n"
+           "and continue with the executeion of 'getchar' in main.\n");
     getchar();
     printf("Main task is over.\n");
     return 0;
